73fc337ef87ec4b18c17996002c846f26a3e7c16
26june <gj.abordo1@gmail.com>
2023-10-18 12:49:16 +0100

    start from scratch

--- src/numbers-data.js
+++ src/numbers-data.js
@@ -1,17 +1,20 @@
 const numbersData = {
+  currentNumber: {
+    id: 1,
+    content: 1,
+  },
+
   generatedNumbers: {
-    1: 1,
-    2: 2,
-    3: 3,
+    2: { id: 2, content: 2 },
   },
   droppableAreas: {
     genArea: {
       id: "genArea",
-      generatedNumbersIds: [1, 2, 3],
+      generatedNumbersIds: [1],
     },
     dropArea: {
       id: "dropArea",
-      generatedNumbersIds: [],
+      generatedNumbersIds: [0, 0, 0, 0, 0],
     },
   },
 };
