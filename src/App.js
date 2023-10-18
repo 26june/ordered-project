4f717240efb63fe72f0682117af29e4e2edade6b
26june <gj.abordo1@gmail.com>
2023-10-18 09:45:25 +0100

    create home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import Home from "./components/Home";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #424549;
+  height: 100vh;
+  width: 100vw;
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container className="App">
+      <Home></Home>
+    </Container>
+  );
 }
 
 export default App;
